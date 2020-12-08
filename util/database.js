7f170db1a108b3b4a26a04d01100d4c0f2778dda
Jiahua Liao <ljh15088131916@gmail.com>
2020-12-08 00:04:50 -0500

    Set Database setting

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const {Sequelize} = require('sequelize');
 
 
-const sequelize = new Sequelize('cisc4900', 'ljh', '3112001944ljh', {dialect: 'mysql', host: 'localhost'});
+// const sequelize = new Sequelize('cisc4900', 'ljh', '3112001944ljh', {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize('vtcabh9w3dplzemu', 'al1zhn90zl0yv42l', 't81kdxjmvzwn82i1', {dialect: 'mysql', host: 'ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'});
 
 module.exports = sequelize;
